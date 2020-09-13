a022e4ed267ff550623c512986d7cae1523e97ff
wonder13662 <wonder13662@gmail.com>
2020-09-13 22:24:53 +0900

    Add firebase project config

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import './plugins/vuetify'
+import * as firebase from 'firebase'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import router from './router'
@@ -9,5 +10,17 @@ Vue.config.productionTip = false
 new Vue({
 	vuetify,
 	router,
-	render: h => h(App)
+	render: h => h(App),
+	created() {
+		firebase.initializeApp({
+			apiKey: "AIzaSyCVC2hf2XrtzbLpSa_BE0BtmCKaxNYvnxk",
+			authDomain: "playground-9a00c.firebaseapp.com",
+			databaseURL: "https://playground-9a00c.firebaseio.com",
+			projectId: "playground-9a00c",
+			storageBucket: "playground-9a00c.appspot.com",
+			messagingSenderId: "140017567845",
+			appId: "1:140017567845:web:e20c8175c58a29533d9e8c",
+			measurementId: "G-EYJZZMVJPX"
+		})
+	}
 }).$mount('#app')
