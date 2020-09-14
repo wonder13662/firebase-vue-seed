2a700aecae5dcf5b4c3ef34c228aa29957fb0966
wonder13662 <wonder13662@gmail.com>
2020-09-14 23:27:19 +0900

    Add firebase and axios

--- src/main.js
+++ src/main.js
@@ -1,26 +1,28 @@
-import Vue from 'vue'
-import './plugins/vuetify'
-import * as firebase from 'firebase'
-import App from './App.vue'
-import vuetify from './plugins/vuetify'
-import router from './router'
+import Vue from 'vue';
+import './plugins/vuetify';
+import * as firebase from 'firebase';
+import App from './App.vue';
+import vuetify from './plugins/vuetify';
+import router from './router';
+import { store } from './store';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
 	vuetify,
 	router,
+	store,
 	render: h => h(App),
 	created() {
 		firebase.initializeApp({
-			apiKey: "AIzaSyCVC2hf2XrtzbLpSa_BE0BtmCKaxNYvnxk",
-			authDomain: "playground-9a00c.firebaseapp.com",
-			databaseURL: "https://playground-9a00c.firebaseio.com",
-			projectId: "playground-9a00c",
-			storageBucket: "playground-9a00c.appspot.com",
-			messagingSenderId: "140017567845",
-			appId: "1:140017567845:web:e20c8175c58a29533d9e8c",
-			measurementId: "G-EYJZZMVJPX"
-		})
-	}
-}).$mount('#app')
+			apiKey: 'AIzaSyCVC2hf2XrtzbLpSa_BE0BtmCKaxNYvnxk',
+			authDomain: 'playground-9a00c.firebaseapp.com',
+			databaseURL: 'https://playground-9a00c.firebaseio.com',
+			projectId: 'playground-9a00c',
+			storageBucket: 'playground-9a00c.appspot.com',
+			messagingSenderId: '140017567845',
+			appId: '1:140017567845:web:e20c8175c58a29533d9e8c',
+			measurementId: 'G-EYJZZMVJPX',
+		});
+	},
+}).$mount('#app');
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -3,24 +3,37 @@
 		<v-card-title>
 			<h1 class="display-1">Login</h1>
 		</v-card-title>
-		<v-card-text>
-			<v-form>
-				<v-text-field label="Username" prepend-icon="mdi-account-circle" />
+		<v-form
+			ref="form"
+			v-model="valid"
+			lazy-validation
+			@submit.prevent="onSignin"
+		>
+			<v-card-text>
 				<v-text-field
-					:type="showPassword ? 'text' : 'password'"
+					label="Email"
+					type="email"
+					v-model="email"
+					:rules="emailRules"
+					prepend-icon="mdi-account-circle"
+				/>
+				<v-text-field
 					label="Password"
+					:type="showPassword ? 'text' : 'password'"
+					v-model="password"
+					:rules="passwordRules"
 					prepend-icon="mdi-lock"
 					:append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
 					@click:append="showPassword = !showPassword"
 				/>
-			</v-form>
-		</v-card-text>
-		<v-divider></v-divider>
-		<v-card-actions>
-			<v-btn color="success">Register</v-btn>
-			<v-spacer></v-spacer>
-			<v-btn color="info">Login</v-btn>
-		</v-card-actions>
+			</v-card-text>
+			<v-divider></v-divider>
+			<v-card-actions>
+				<v-btn color="success">Register</v-btn>
+				<v-spacer></v-spacer>
+				<v-btn color="info" type="submit" :loading="loading">Login</v-btn>
+			</v-card-actions>
+		</v-form>
 	</v-card>
 </template>
 
@@ -29,10 +42,60 @@ export default {
 	name: 'LoginPage',
 	data() {
 		return {
-			showPassword: false
-		}
-	}
-}
+			valid: true,
+			showPassword: false,
+			email: '',
+			emailRules: [
+				v => !!v || 'E-mail is required',
+				v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
+			],
+			password: '',
+			passwordRules: [
+				v => !!v || 'Password is required',
+				v =>
+					(v && v.length <= 10) || 'Password must be less than 10 characters',
+			],
+		};
+	},
+	computed: {
+		user() {
+			return this.$store.getters.user;
+		},
+		error() {
+			return this.$store.getters.error;
+		},
+		loading() {
+			return this.$store.getters.loading;
+		},
+	},
+	methods: {
+		onSignin() {
+			console.log('onSignin');
+			this.validate();
+			// 0. submit 이벤트로 이곳에 진입해야 한다.
+			// 1. 입력폼의 데이터들의 유효성을 검사한다.
+			// 2. 데이터가 유효하다면 로그인을 진행한다.
+			/*
+      this.$store.dispatch("signUserIn", {
+        email: this.email,
+        password: this.password,
+			});
+			*/
+		},
+		onDismissed() {
+			this.$store.dispatch('clearError');
+		},
+		validate() {
+			this.$refs.form.validate();
+		},
+		reset() {
+			this.$refs.form.reset();
+		},
+		resetValidation() {
+			this.$refs.form.resetValidation();
+		},
+	},
+};
 </script>
 
 <style></style>
