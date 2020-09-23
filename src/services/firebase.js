7de32d697b8f08b48f877c06974ba633ea65c345
wonder13662 <wonder13662@gmail.com>
2020-09-23 23:59:14 +0900

    Remove unused logic in firebase module

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,46 +1,17 @@
 import * as firebase from 'firebase'
 
-// 1. Realtime Database
-// 2. Cloud Firestore
+// TODO 0. Authentication
+// TODO 1. Realtime Database
+// TODO 2. Cloud Firestore
 
 const module = {
-  setUser(user) {
-    const userListRef = firebase.database().ref('presence')
-    const myUserRef = userListRef.push()
-
-    firebase
-      .database()
-      .ref('.info/connected')
-      .on('value', snap => {
-        if (snap.val()) {
-          // if we lose network then remove this user from the list
-          myUserRef.onDisconnect().remove()
-          // set user's online status
-          let presenceObject = { user, status: 'online' }
-          myUserRef.set(presenceObject)
-        } else {
-          // client has lost network
-          let presenceObject = { user, status: 'offline' }
-          myUserRef.set(presenceObject)
-        }
-      })
-  },
-  async createUserWithEmailAndPassword({ email, password, username }) {
+  async createUserWithEmailAndPassword({ email, password }) {
+    // TODO 유저의 추가 정보를 넣을 수 있도록 해야 함
+    // TODO 여러 명의 유저들을 한꺼번에 넣으려면?
     try {
-      const auth = await firebase
+      await firebase
         .auth()
         .createUserWithEmailAndPassword(email, password)
-      await firebase
-        .database()
-        .ref('users')
-        .child(auth.user.uid)
-        .set({
-          name: username,
-        })
-      this.setUser({
-        id: auth.user.uid,
-        username: username,
-      })
       return await Promise.resolve()
     } catch (err) {
       return await Promise.reject(err)
@@ -51,20 +22,15 @@ const module = {
       const auth = await firebase
         .auth()
         .signInWithEmailAndPassword(email, password)
-      await firebase
-        .database()
-        .ref('users')
-        .child(auth.user.uid)
-        .once('value', () => {
-          this.setUser({
-            id: auth.user.uid,
-            username: auth.user.email, // TODO 왜 이메일로 설정될까? username이어야 할 것 같은데...
-          })
-        })
+
+      console.log('auth:', auth)
       return await Promise.resolve()
     } catch (err) {
       return await Promise.reject(err)
     }
+  },
+  onAuthChanged(callback) {
+    firebase.auth().onAuthStateChanged((user) => callback(user))
   },
 }
 
