fff350d90cce3a0634226484ee529fad80dfcc3f
wonder13662 <wonder13662@gmail.com>
2020-09-24 10:29:22 +0900

    Detach AppNav from App

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,11 @@
 <template>
 	<v-app>
-		<v-app-bar app color="primary" dark>
-			<v-toolbar-title>Vuetify Dashboard</v-toolbar-title>
-			<v-spacer></v-spacer>
-			<v-btn
-				v-for="link in links"
-				:key="`${link.label}-header-link`"
-				text
-				rounded
-				:to="link.url"
-				>{{ link.label }}</v-btn
-			>
-			<v-btn @click="toggleTheme" text rounded>Toggle Theme</v-btn>
-		</v-app-bar>
+		<app-nav/>
 		<v-main>
 			<router-view></router-view>
 		</v-main>
 		<v-footer color="primary lighten-1" padless>
 			<v-layout justify-center wrap>
-				<v-btn
-					v-for="link in links"
-					:key="`${link.label}-footer-link`"
-					color="white"
-					text
-					rounded
-					class="my-2"
-					:to="link.url"
-					>{{ link.label }}</v-btn
-				>
 				<v-flex primary lighten-2 py-4 text-center white--text xs12>
 					{{ new Date().getFullYear() }} —
 					<strong>Vuetify Dashboard</strong>
@@ -38,35 +16,10 @@
 </template>
 
 <script>
+import AppNav from '@/components/AppNav'
+
 export default {
+  components: { AppNav },
   name: 'App',
-  data() {
-    return {
-      links: [
-        {
-          label: 'Home',
-          url: '/',
-        },
-        {
-          label: 'Login',
-          url: '/login',
-        },
-        {
-          label: 'Signup',
-          url: '/signup',
-        },
-        {
-          label: 'Dashboard',
-          url: '/dashboard',
-        },
-      ],
-    }
-  },
-  methods: {
-    toggleTheme() {
-      this.$vuetify.theme.themes.dark.anchor = '#41B883'
-      this.$vuetify.theme.dark = !this.$vuetify.theme.dark
-    },
-  },
 }
 </script>
---
+++ src/components/AppNav.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-app-bar app color="primary" dark>
+    <v-toolbar-title>Vuetify Dashboard</v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-btn text rounded :to="'/home'">Home</v-btn>
+    <v-btn v-if="!signedIn" text rounded :to="'/login'">Login</v-btn>
+    <v-btn v-if="signedIn" text rounded :to="'/dashboard'">Dashboard</v-btn>
+    <v-btn v-if="signedIn" text rounded @click="signOut">Sign Out</v-btn>
+  </v-app-bar>
+</template>
+
+<script>
+// TODO
+// # AppNav 메뉴 보이는 상태
+// 1. 로그인 - 일반 유저
+// 2. 로그인 - Admin
+// 3. 로그아웃
+// 4. requiresAuth
+
+// # 리팩토링
+// 1. login? signIn?
+// 2. logOut? signOut?
+
+export default {
+  computed: {
+    signedIn() {
+      return this.$store.getters['auth/signedIn']
+    },
+  },
+  methods: {
+    signOut() {
+      console.log('signOut')
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
