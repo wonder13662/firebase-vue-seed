6cebb54b0d74034fa6f0eb395d5e6aa2d1116428
wonder13662 <wonder13662@gmail.com>
2020-09-24 10:29:37 +0900

    Add TODO List

--- src/main.js
+++ src/main.js
@@ -19,5 +19,13 @@ new Vue({
       this.$store.commit('auth/SET_USER_DATA', { email: user.email })
     })
     // TODO router-guard 만들기
+    // 1. 로그인 상태
+    // 1-1. Dashboard 보임
+    // 1-2. Login/SignUp 버튼 숨김
+    // 1-3. LogOut 버튼 보임
+    // 2. 로그아웃 상태
+    // 2-1. Dashboard 숨김
+    // 2-2. Login/SignUp 버튼 보임
+    // 2-3. LogOut 버튼 보임
   },
 }).$mount('#app')
