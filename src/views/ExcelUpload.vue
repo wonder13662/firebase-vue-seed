be4247031b57482559676ef97ef91189de054862
wonder13662 <wonder13662@gmail.com>
2020-09-30 09:27:07 +0900

    Add excel view

--- src/views/ExcelUpload.vue
+++ src/views/ExcelUpload.vue
@@ -15,6 +15,39 @@
       justify="space-around">
     <v-file-input label="Excel File Input" @change="change"></v-file-input>
     </v-row>
+    <v-row>
+      <v-simple-table v-if="items.length > 0">
+        <template v-slot:default>
+          <thead>
+            <tr>
+              <th class="text-left">
+                이름
+              </th>
+              <th class="text-left">
+                Email
+              </th>
+              <th class="text-left">
+                그룹
+              </th>
+              <th class="text-left">
+                팀
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr
+              v-for="item in items"
+              :key="item.email"
+            >
+              <td>{{ item['이름'] }}</td>
+              <td>{{ item.email }}</td>
+              <td>{{ item['그룹'] }}</td>
+              <td>{{ item['팀'] }}</td>
+            </tr>
+          </tbody>
+        </template>
+      </v-simple-table>
+    </v-row>
   </div>
 </template>
 
@@ -27,9 +60,11 @@ export default {
       excelTemplate: {
         data: [['이름', 'email', '그룹', '팀']],
       },
+      items: [],
     }
   },
   methods: {
+    // TODO 업로드한 파일의 내역을 화면에 보여주기
     exportExcel() {
       console.log('exportExcel')
       // https://github.com/SheetJS/sheetjs/blob/master/demos/vue/pages/index.vue
@@ -53,6 +88,7 @@ export default {
         // const json = XLSX.utils.sheet_to_json(worksheet, {header:1})
         const json = XLSX.utils.sheet_to_json(worksheet)
         console.log('json:', json)
+        this.items = json
       }
       reader.readAsArrayBuffer(file)
     },
